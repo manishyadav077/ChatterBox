ddf3489d98ca1a5ac20e913e776faf1edd049db6
manishyadav077 <136573039+manishyadav077@users.noreply.github.com>
2025-01-20 15:53:46 +0545

    updated application

--- app/api/create/route.js
+++ app/api/create/route.js
@@ -1,10 +1,8 @@
 import { StreamChat } from "stream-chat";
 import { clerkClient } from "@clerk/nextjs/dist/types/server";
 
-const api_key = "qr6ebvg5zyx9";
-const api_secret =
-  "bzfsa8s6nvg3m3vy7rspj2tsvakan5p6zppk77c2nsc9zz5apfqdfqevvcvg28gj";
-const user_id = "";
+const api_key = process.env.API_KEY;
+const api_secret = process.env.API_SECRET;
 
 export async function POST(request) {
   const serverClient = StreamChat.getInstance(api_key, api_secret);
@@ -13,23 +11,23 @@ export async function POST(request) {
 
   const client = await clerkClient();
 
-  await serverClient.upsertUser({id: user.data.id})
+  await serverClient.upsertUser({ id: user.data.id });
 
   await client.users.updateUserMetadata(user.data.id, {
     token: token,
   });
 
-  const slugs = ["python", "js", "react", "node", "machine-learning"]
+  const slugs = ["python", "js", "react", "node", "machine-learning"];
 
-  slugs.forEach(async(item)=> {
+  slugs.forEach(async (item) => {
     const channel = client.channel("messaging", item, {
-        image: "https://getstream.io/random_png/?name=react",
-        name: "Discussion",
-        created_by_id: user.data.id
-      });
-      await channel.create()
-      channel.addMembers([user.data.id])
-  })
+      image: "https://getstream.io/random_png/?name=react",
+      name: "Discussion",
+      created_by_id: user.data.id,
+    });
+    await channel.create();
+    channel.addMembers([user.data.id]);
+  });
 
   return Response.json({ message: "Hello World" });
 }
